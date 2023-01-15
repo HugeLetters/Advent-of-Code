cb2de3651e713d0a4fa7a65394b421e5f1259e99
HugeLetters <ckuka71@gmail.com>
2023-01-15 21:12:14 +0300

    got 16p2 working in 8 hours

--- solution/16.js
+++ solution/16.js
@@ -12,9 +12,12 @@ const solution = (input) => {
 
     const graph = parseInput(input);
     const distanceMap = transformDistanceMap(graph, FWPath(graph));
-    const clearGraph = parseGraph(graph, distanceMap, "AA")
-    // const result = findBestPath(clearGraph, 30, "START", clearGraph["START"].refs);
-    const result = findBestPathDuo(testGraph, [100, 100], ["START", "START"], testGraph["START"].refs);
+    const clearGraph = parseGraph(graph, distanceMap, "AA");
+    // const result = findBestPath(clearGraph, 30, "START");
+    // const result = findBestPath(testGraph, 100, "START");
+
+    const result = findBestPathDuo(clearGraph, 26);
+    // const result = findBestPathDuo(testGraph, 100);
 
     console.timeEnd(1);
     return result
@@ -89,51 +92,116 @@ const parseGraph = (graph, distanceMap, start) => {
     return newGraph;
 }
 
-const findBestPath = (graph, limit, start, unvisited) => {
-    const nodeList = Object.keys(unvisited);
+const optimizeGraph = (graph, limit) => {
+    const nodeList = Object.keys(graph).sort((a) => a == 'START' ? -1 : 1);
+    const nodeMap = {};
+    nodeList.forEach((node, i) => { nodeMap[node] = i });
 
-    return nodeList.reduce((max, node) => {
-        const timer = limit - graph[start].refs[node];
-        if (timer >= 0) {
-            const { [node]: _, ...restUnvisited } = unvisited;
-            const pressure = timer * graph[node].value +
-                (restUnvisited != {} ? findBestPath(graph, timer, node, restUnvisited) : 0);
-            max = Math.max(max, pressure);
+    const newGraph = nodeList.map(node => {
+        const timer = limit - (graph['START'].refs[node] || 0);
+        const refList = Object.keys(graph[node].refs)
+            .sort((a, b) => {
+                const timerA = timer - graph[node].refs[a];
+                const timerB = timer - graph[node].refs[b];
+                return timerB * graph[b].value - timerA * graph[a].value;
+            });
+        const refPriority = refList.map(ref => nodeMap[ref]);
+        const refDistance = {};
+        refList.forEach(ref =>
+            refDistance[nodeMap[ref]] = graph[node].refs[ref]
+        );
+        const refBitmap = refList.reduce((bitmap, ref) => {
+            bitmap = bitmap ^ (1 << nodeMap[ref]);
+            return bitmap
+        }, 0)
+        return {
+            value: graph[node].value,
+            refDistance,
+            refBitmap,
+            refPriority,
         }
-        return max
-    }, 0);
+    });
+
+    return newGraph
 }
 
-const findBestPathDuo = (graph, limit, start, unvisited) => {
+const findBestPath = (graph, limit) => {
+    const newGraph = optimizeGraph(graph, limit);
+    return findBestPathStep(newGraph, limit, 0, 1, 0);
+}
+
+const findBestPathStep = (graph, limit, start, visitedBitmap, currentMax) => {
+
+    const maxPossibleSum = graph.reduce((sum, node, nodeKey) => {
+        if (visitedBitmap & (1 << nodeKey)) return sum;
+        const timer = limit - graph[start].refDistance[nodeKey];
+        const pressure = timer > 0 ? timer * node.value : 0;
+        return sum + pressure;
+    }, 0);
+    if (maxPossibleSum <= currentMax) return 0;
+
+    if (!(~visitedBitmap & graph[start].refBitmap)) return maxPossibleSum;
+
+    return graph[start].refPriority.reduce((max, refNodeKey) => {
+        const nodeBitmap = 1 << refNodeKey;
+        if (visitedBitmap & nodeBitmap) return max;
+        const timer = limit - graph[start].refDistance[refNodeKey];
+        if (timer <= 0) return max;
+        const newVisited = visitedBitmap | nodeBitmap;
+        const pressure = timer * graph[refNodeKey].value;
+        return Math.max(max, pressure +
+            ((timer > 2)
+                ? findBestPathStep(graph, timer, refNodeKey, newVisited, max - pressure)
+                : 0));
+    }, 0)
+}
+
+const findBestPathDuo = (graph, limit) => {
+    const newGraph = optimizeGraph(graph, limit);
+    return findBestPathDuoStep(newGraph, [limit, limit], [0, 0], 1, 0)
+}
+
+const findBestPathDuoStep = (graph, limit, start, visitedBitmap, currentMax) => {
+
+    const maxPossibleSum = graph.reduce((sum, node, nodeKey) => {
+        if (visitedBitmap & (1 << nodeKey)) return sum;
+        const timer = limit.map((_, i) => limit[i] - graph[start[i]].refDistance[nodeKey]);
+        const pressure = timer.map((time) => time > 0 ? time * node.value : 0);
+        return sum + Math.max(...pressure);
+    }, 0);
+    if (maxPossibleSum <= currentMax) return 0;
+
+    if (!(~visitedBitmap & (graph[start[0]].refBitmap | graph[start[1]].refBitmap))) return maxPossibleSum;
+
     let max = 0;
-    const nodeList = Object.keys(unvisited);
-    const [limit1, limit2] = limit;
-    const [start1, start2] = start;
-    nodeList.forEach(node1 => {
-        const timer1 = limit1 - graph[start1].refs[node1];
-        if (timer1 >= 0) {
-
-            const { [node1]: _, ...restUnvisited1 } = unvisited;
-            const nodeList1 = Object.keys(restUnvisited1);
-
-            const pressure1 = timer1 * graph[node1].value +
-                (restUnvisited1 != {} ? findBestPathDuo(graph, [timer1, limit2], [node1, start2], restUnvisited1) : 0);;
-
-            const pressure2 = nodeList1.reduce((max2, node2) => {
-                const timer2 = limit2 - graph[start2].refs[node2];
-                if (timer2 >= 0) {
-                    const { [node2]: _, ...restUnvisited2 } = restUnvisited1;
-                    const pressure2 = timer2 * graph[node2].value +
-                        (restUnvisited2 != {} ? findBestPathDuo(graph, [timer1, timer2], [node1, node2], restUnvisited2) : 0);
-                    max2 = Math.max(max2, pressure2);
-                }
-                return max2
-            }, 0);
-            max = Math.max(max, pressure1 + pressure2)
-        }
+    graph[start[0]].refPriority.forEach(refNodeKey => {
+        const nodeBitmap = 1 << refNodeKey;
+        if (visitedBitmap & nodeBitmap) return null;
+        const timer = limit[0] - graph[start[0]].refDistance[refNodeKey];
+        if (timer <= 0) return null;
+        const newVisited = visitedBitmap | nodeBitmap;
+        const pressure = timer * graph[refNodeKey].value;
+        max = Math.max(max, pressure +
+            ((timer > 2)
+                ? findBestPathDuoStep(graph, [timer, limit[1]], [refNodeKey, start[1]], newVisited, max - pressure)
+                : 0))
+    })
+    graph[start[1]].refPriority.forEach(refNodeKey => {
+        const nodeBitmap = 1 << refNodeKey;
+        if (visitedBitmap & nodeBitmap) return null;
+        const timer = limit[1] - graph[start[1]].refDistance[refNodeKey];
+        if (timer <= 0) return null;
+        const newVisited = visitedBitmap | nodeBitmap;
+        const pressure = timer * graph[refNodeKey].value;
+        max = Math.max(max, pressure +
+            ((timer > 2)
+                ? findBestPathDuoStep(graph, [limit[0], timer], [start[0], refNodeKey], newVisited, max - pressure)
+                : 0))
     })
     return max;
 }
+
+
 
 const testGraph = {
     "START": { value: 0, refs: { "A": 99, "B": 1, "C": 2 } },
